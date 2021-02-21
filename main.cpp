a473aea12c2ec21f8267c25119ba8c4b850935b3
megaseg01 <megaserg01@mail.ru>
2021-02-21 13:51:11 +0300

    fixed includes in CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "include/Board.h"
-#include "include/Deck.h"
+#include "Board.h"
+#include "Deck.h"
 int main() {
     Deck deck("wagons.txt", "short_routes.txt", "long_routes.txt");
     Board board("paths.txt");
