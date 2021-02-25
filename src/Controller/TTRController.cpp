1a406e1f2304b0dcbb6fb6ecae46e686ed9aef10
megaseg01 <megaserg01@mail.ru>
2021-02-25 22:45:26 +0300

    initial branch commit

---
+++ src/Controller/TTRController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by megaserg01 on 25.02.2021.
+//
+
+#include "TTRController.h"
---
+++ src/Controller/TTRController.h
@@ -0,0 +1,10 @@
+//
+// Created by megaserg01 on 25.02.2021.
+//
+
+#ifndef TTR_TTRCONTROLLER_H
+#define TTR_TTRCONTROLLER_H
+
+class TTRController {};
+
+#endif  // TTR_TTRCONTROLLER_H
