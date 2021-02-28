11de51700e9738c9377e748a9fd59642afedab27
megaseg01 <megaserg01@mail.ru>
2021-02-28 16:32:37 +0300

    some Turn fixes and Controller implementations

--- src/Controller/TTRController.cpp
+++ src/Controller/TTRController.cpp
@@ -4,7 +4,6 @@
 
 #include "Controller/TTRController.h"
 
-
 void TTRController::start_game(int number_of_players) {
     game = new Game(number_of_players);
     game->start_game();
@@ -28,13 +27,19 @@ void TTRController::get_routes() {
     current_turn = new DrawCardFromDeck();
     game->make_move(current_turn);
 }
+
 TTRController::~TTRController() {
     delete game;
     delete current_turn;
 }
+
 void TTRController::set_color_to_build_path(const WagonCard &w) {
-    if(auto p = dynamic_cast<BuildPath*>(current_turn)){
+    if (auto p = dynamic_cast<BuildPath *>(current_turn); p) {
         p->set_wagons(game->cards_with_suitable_color(w));
         game->make_move(p);
     }
+}
+
+const std::vector<WagonCard> &TTRController::get_current_player_cards() {
+    return game->players[game->active_player].wagon_cards;
 }
