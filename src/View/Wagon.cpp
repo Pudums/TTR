eef1961b50e0a2f9be3706acc6a2946323af2a12
Max <max_mail2002@bk.ru>
2021-02-28 18:29:54 +0300

    brush added for a wagon

--- src/View/Wagon.cpp
+++ src/View/Wagon.cpp
@@ -1,6 +1,12 @@
 #include "Wagon.h"
+#include <QBrush>
 
 Wagon::Wagon(QVector<QPointF> &points) {
 	QPolygonF wagon_polygon(points);
 	setPolygon(wagon_polygon);
+
+	QBrush brush;
+	brush.setStyle(Qt::SolidPattern);
+	brush.setColor(Qt::lightGray);
+	setBrush(brush);
 }
